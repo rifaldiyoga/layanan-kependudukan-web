23b62d86899399914bcdf91ce80c9e9d01640c00
Rifaldi Yoga <rifaldi.yoga@bee.id>
2023-09-27 14:27:33 +0700

    init

---
+++ src/views/Dashboard/Tables/index.js
@@ -0,0 +1,20 @@
+// Chakra imports
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import Authors from "./components/Authors";
+import Projects from "./components/Projects";
+import { tablesTableData, dashboardTableData } from "variables/general";
+
+function Tables() {
+    return (
+        <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
+            <Authors
+                title={"Authors Table"}
+                captions={["Author", "Function", "Status", "Employed", ""]}
+                data={tablesTableData}
+            />
+        </Flex>
+    );
+}
+
+export default Tables;
